6bda5e064f63819a62c7a2898a59e11403aea792
Sodiq Al-ameen <sodiq@tm30.net>
2024-08-06 12:41:35 +0100

    see

--- src/MainComponents/LandingPage/Footer.jsx
+++ src/MainComponents/LandingPage/Footer.jsx
@@ -1,10 +1,56 @@
 import React from "react";
 import styled from "styled-components";
 import { ReactComponent as CopyRight } from "../../assets/copyright.svg";
+import { ReactComponent as Falcon } from "../../assets/footfalcon.svg";
+import { ReactComponent as Insta } from "../../assets/insta.svg";
+import { ReactComponent as Linkedin } from "../../assets/linkedin.svg";
+import { ReactComponent as Facebook } from "../../assets/facebook.svg";
+import { ReactComponent as X } from "../../assets/x.svg";
 
 const Footer = () => {
   return (
     <Flex>
+      <div className="foothead">
+        <div className="logo">
+          <Falcon />
+          <div className="socials">
+            <Insta />
+            <Linkedin />
+            <X />
+            <Facebook />
+          </div>
+        </div>
+        <div className="prodindustry">
+          <div className="titles">
+            <span className="head">PRODUCT</span>
+            <div className="prices">
+              <span className="item">Pricing</span>
+              <span className="item">Location-Based Targeting</span>
+              <span className="item">Real-Time Tracking</span>
+              <span className="item">Advanced Analytics</span>
+              <span className="item">Campaign Managemen</span>
+            </div>
+          </div>
+          <div className="titles">
+            <span className="head">INDUSTRIES</span>
+            <div className="prices">
+              <span className="item">Retail</span>
+              <span className="item">Healthcare</span>
+              <span className="item">Education</span>
+              <span className="item">Security</span>
+              <span className="item">Explore all</span>
+            </div>
+          </div>
+          <div className="titles">
+            <span className="head">RESOURCES</span>
+            <div className="prices">
+              <span className="item">About us</span>
+              <span className="item">Contact us</span>
+              <span className="item">Privacy & Terms</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="line" />
       <div className="foremost">
         <span className="copy">
@@ -28,6 +74,52 @@ const Flex = styled.div`
   padding-top: 70px;
   padding-bottom: 10px;
   gap: 20px;
+  .foothead {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-block: 20px;
+    color: #ffffff;
+    .logo {
+      display: flex;
+      flex-direction: column;
+      gap: 50px;
+      .socials {
+        display: flex;
+        flex-direction: row;
+        gap: 30px;
+      }
+    }
+    .prodindustry {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      gap: 130px;
+      .titles {
+        display: flex;
+        flex-direction: column;
+        gap: 50px;
+        .head {
+          font-size: 15px;
+          font-weight: 700;
+          line-height: 22.5px;
+          text-align: left;
+        }
+        .prices {
+          display: flex;
+          flex-direction: column;
+          gap: 7px;
+          .item {
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 19.36px;
+            text-align: left;
+          }
+        }
+      }
+    }
+  }
   .line {
     width: 100%;
     height: 1px;
