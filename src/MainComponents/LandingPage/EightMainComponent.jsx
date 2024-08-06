7ae2b68d35e29f343cf67fbed3e2beec8187e908
Sodiq Al-ameen <sodiq@tm30.net>
2024-08-06 10:53:02 +0100

    see

--- src/MainComponents/LandingPage/EightMainComponent.jsx
+++ src/MainComponents/LandingPage/EightMainComponent.jsx
@@ -3,13 +3,22 @@ import styled from "styled-components";
 import Map from "../../assets/map.png";
 
 const EightMainComponent = () => {
-  return <Flex></Flex>;
+  return (
+    <Flex>
+      <div className="right">
+        <span className="campaign">
+          Choose <span className="grow">De - Faucon.</span>for an
+        </span>
+        <span>industry-leading experience</span>
+      </div>
+    </Flex>
+  );
 };
 
 const Flex = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: flex-end;
   align-items: center;
   padding-top: 170px;
   padding-inline: 65px;
@@ -18,6 +27,27 @@ const Flex = styled.div`
   background-image: url(${Map});
   background-size: cover;
   background-repeat: no-repeat;
+
+  .right {
+    display: flex;
+    flex-direction: column;
+    font-size: 35px;
+    font-weight: 600;
+    line-height: 53px;
+    letter-spacing: -0.03em;
+    padding-right: 390px;
+    color: #ffffff;
+    .campaign {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 10px;
+      .grow {
+        color: #dd4a5d;
+      }
+    }
+  }
 `;
 
 export default EightMainComponent;
--- src/MainComponents/LandingPage/FourthMainComponent.jsx
+++ src/MainComponents/LandingPage/FourthMainComponent.jsx
@@ -157,6 +157,101 @@ const Flex = styled.div`
       }
     }
   }
+  @media screen and (max-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+    padding-inline: 65px;
+    overflow-x: hidden;
+    .cover {
+      border-bottom: 1px solid #ededed;
+      display: flex;
+      flex-direction: column;
+      padding-block: 40px;
+      width: 100%;
+      gap: 80px;
+      .demo {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        .book {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          width: 143px;
+          height: 43px;
+          padding: 13px 12px 13px 12px;
+          gap: 8px;
+          border: 1px solid #1a87d7;
+          border-radius: 25px;
+          opacity: 0px;
+          background: #1a87d7;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 20px;
+          letter-spacing: 0.02em;
+          color: #ffffff;
+          outline: none;
+          cursor: pointer;
+          box-shadow: 0px 0px 0px 1px #1a87d7;
+          /* box-shadow: 0px 1px 1px 0px #0000001a; */
+        }
+      }
+      .down {
+        display: flex;
+        flex-direction: column;
+        padding-inline: 65px;
+        /* padding-top: 60px; */
+        align-items: center;
+        justify-content: center;
+        height: 40vh;
+        gap: 10px;
+        .ourservices {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          gap: 5px;
+          width: 126px;
+          height: 32px;
+          padding: 6.56px 13.8px 7.44px 10.2px;
+          border-radius: 29.07px;
+          border: 1.16px 0px 0px 0px;
+          opacity: 0px;
+          font-size: 13px;
+          font-weight: 500;
+          line-height: 18.15px;
+          color: #28385c;
+          background: linear-gradient(
+            83.05deg,
+            rgba(121, 170, 205, 0.08) 17.7%,
+            rgba(61, 85, 103, 0.08) 104.55%
+          );
+          border: 1.16px solid #ededed;
+        }
+        .right {
+          display: flex;
+          flex-direction: column;
+          font-size: 35px;
+          font-weight: 600;
+          line-height: 53px;
+          letter-spacing: -0.03em;
+          color: #28385c;
+          .campaign {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            gap: 10px;
+            .grow {
+              color: #dd4a5d;
+            }
+          }
+        }
+      }
+    }
+  }
 `;
 
 export default FourthMainComponent;
