c78cb5c46ceb998757fce9fc7d02751876c4c049
Sodiq Al-ameen <sodiq@tm30.net>
2024-09-19 15:41:40 +0100

    see

--- src/MainComponents/AdminClients/ClientLocationDetails.jsx
+++ src/MainComponents/AdminClients/ClientLocationDetails.jsx
@@ -883,9 +883,25 @@ const ClientLocationDetails = ({ title }) => {
                       <option value="" disabled>
                         Select Resumption Time
                       </option>
+                      <option value="06:00:00">6:00 A.M</option>
+                      <option value="07:00:00">7:00 A.M</option>
+                      <option value="08:00:00">8:00 A.M</option>
                       <option value="09:00:00">9:00 A.M</option>
                       <option value="10:00:00">10:00 A.M</option>
                       <option value="11:00:00">11:00 A.M</option>
+                      <option value="12:00:00">12:00 P.M</option>
+                      <option value="13:00:00">01:00 P.M</option>
+                      <option value="14:00:00">02:00 P.M</option>
+                      <option value="15:00:00">03:00 P.M</option>
+                      <option value="16:00:00">04:00 P.M</option>
+                      <option value="17:00:00">05:00 P.M</option>
+                      <option value="18:00:00">06:00 P.M</option>
+                      <option value="19:00:00">07:00 P.M</option>
+                      <option value="20:00:00">08:00 P.M</option>
+                      <option value="21:00:00">09:00 P.M</option>
+                      <option value="22:00:00">10:00 P.M</option>
+                      <option value="23:00:00">11:00 P.M</option>
+                      <option value="00:00:00">12:00 A.M</option>
                     </select>
                   </div>
                   <div className="projectnamethree">
@@ -899,15 +915,31 @@ const ClientLocationDetails = ({ title }) => {
                       <option value="" disabled>
                         Select Closing Time
                       </option>
+                      <option value="06:00:00">6:00 A.M</option>
+                      <option value="07:00:00">7:00 A.M</option>
+                      <option value="08:00:00">8:00 A.M</option>
+                      <option value="09:00:00">9:00 A.M</option>
+                      <option value="10:00:00">10:00 A.M</option>
+                      <option value="11:00:00">11:00 A.M</option>
+                      <option value="12:00:00">12:00 P.M</option>
+                      <option value="13:00:00">01:00 P.M</option>
+                      <option value="14:00:00">02:00 P.M</option>
                       <option value="15:00:00">03:00 P.M</option>
                       <option value="16:00:00">04:00 P.M</option>
                       <option value="17:00:00">05:00 P.M</option>
+                      <option value="18:00:00">06:00 P.M</option>
+                      <option value="19:00:00">07:00 P.M</option>
+                      <option value="20:00:00">08:00 P.M</option>
+                      <option value="21:00:00">09:00 P.M</option>
+                      <option value="22:00:00">10:00 P.M</option>
+                      <option value="23:00:00">11:00 P.M</option>
+                      <option value="00:00:00">12:00 A.M</option>
                     </select>
                   </div>
                 </div>
                 <div className="filling">
                   <div className="projectname">
-                    <span className="name">Resumption Grace Period</span>
+                    <span className="name">Attendance Grace Period</span>
                     <select
                       name="minutesToAdd"
                       value={assign.minutesToAdd || ""}
