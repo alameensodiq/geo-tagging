62baa575aa1279f14357421e1d6a67a661ce49bc
Sodiq Al-ameen <sodiq@tm30.net>
2024-10-09 15:39:14 +0100

    see

--- src/MainComponents/LandingPage/FirstIndustryComponent.jsx
+++ src/MainComponents/LandingPage/FirstIndustryComponent.jsx
@@ -233,6 +233,316 @@ const Flex = styled.div`
       width: 60%;
     }
   }
+  @media screen and (max-width: 1200px) {
+    .cover {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      .top {
+        display: flex;
+        flex-direction: column;
+        gap: 18px;
+        align-items: flex-start;
+        .retailer {
+          font-size: 15px;
+          font-style: italic;
+          font-weight: 500;
+          line-height: 18.15px;
+          text-align: left;
+          color: #1a87d7;
+        }
+        .divcover {
+          display: flex;
+          flex-direction: column;
+          font-size: 22px;
+          font-weight: 600;
+          line-height: 50.4px;
+          text-align: left;
+          color: #28385c;
+        }
+        .button {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          gap: 10px;
+          align-items: center;
+          width: 220px;
+          height: 28px;
+          padding: 4px 9px 4px 0px;
+          border: 1px solid #28385c;
+          border-radius: 20px;
+          opacity: 0px;
+          background: #28385c;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 16.94px;
+          color: #ffffff;
+        }
+        .anime {
+          display: flex;
+          flex-direction: row;
+          /* font-family: Sora; */
+          font-size: 80px;
+          font-weight: 600;
+          line-height: 100.8px;
+          letter-spacing: -0.03em;
+          gap: 20px;
+          .boosts {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            /* font-family: Sora; */
+            font-size: 67px;
+            font-weight: 600;
+            line-height: 100.8px;
+            letter-spacing: -0.03em;
+            color: #28385c;
+          }
+          .productive {
+            font-size: 67px;
+            color: #dd4a5d;
+            font-weight: 600;
+            line-height: 100.8px;
+            letter-spacing: -0.03em;
+          }
+        }
+        .statements {
+          display: flex;
+          flex-direction: column;
+          width: auto;
+          gap: 0px;
+          .statement {
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 31px;
+            color: #28385c;
+          }
+        }
+        .stores {
+          display: flex;
+          flex-direction: row;
+          gap: 15px;
+        }
+        .avatar {
+          display: flex;
+          flex-direction: row;
+          gap: 5px;
+          align-items: center;
+          .downloads {
+            font-size: 13px;
+            font-weight: 500;
+            line-height: 19.36px;
+            color: #28385c;
+          }
+        }
+        .book {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          width: 143px;
+          height: 43px;
+          padding: 13px 12px 13px 12px;
+          gap: 8px;
+          border: 1px solid #27375b;
+          border-radius: 25px;
+          opacity: 0px;
+          background: #27375b;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 20px;
+          letter-spacing: 0.02em;
+          color: #ffffff;
+          outline: none;
+          cursor: pointer;
+          box-shadow: 0px 0px 0px 1px #27375b;
+          /* box-shadow: 0px 1px 1px 0px #0000001a; */
+        }
+      }
+      .side {
+        .pictures {
+          width: ${(props) => (props?.reduce ? "620px" : "590px")};
+          height: ${(props) => (props?.reduce ? "470px" : "auto")};
+        }
+      }
+    }
+    .bottom {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+      .trusted {
+        font-size: 17px;
+        font-weight: 400;
+        line-height: 24.2px;
+        text-align: left;
+        color: #5a6376cc;
+      }
+      .sliding {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        width: 60%;
+      }
+    }
+  }
+  @media screen and (max-width: 900px) {
+    .cover {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      .top {
+        display: flex;
+        flex-direction: column;
+        gap: 18px;
+        align-items: flex-start;
+        .retailer {
+          font-size: 15px;
+          font-style: italic;
+          font-weight: 500;
+          line-height: 18.15px;
+          text-align: left;
+          color: #1a87d7;
+        }
+        .divcover {
+          display: flex;
+          flex-direction: column;
+          font-size: 22px;
+          font-weight: 600;
+          line-height: 50.4px;
+          text-align: left;
+          color: #28385c;
+        }
+        .button {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          gap: 10px;
+          align-items: center;
+          width: 220px;
+          height: 28px;
+          padding: 4px 9px 4px 0px;
+          border: 1px solid #28385c;
+          border-radius: 20px;
+          opacity: 0px;
+          background: #28385c;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 16.94px;
+          color: #ffffff;
+        }
+        .anime {
+          display: flex;
+          flex-direction: row;
+          /* font-family: Sora; */
+          font-size: 80px;
+          font-weight: 600;
+          line-height: 100.8px;
+          letter-spacing: -0.03em;
+          gap: 20px;
+          .boosts {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            /* font-family: Sora; */
+            font-size: 67px;
+            font-weight: 600;
+            line-height: 100.8px;
+            letter-spacing: -0.03em;
+            color: #28385c;
+          }
+          .productive {
+            font-size: 67px;
+            color: #dd4a5d;
+            font-weight: 600;
+            line-height: 100.8px;
+            letter-spacing: -0.03em;
+          }
+        }
+        .statements {
+          display: flex;
+          flex-direction: column;
+          width: auto;
+          gap: 0px;
+          .statement {
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 31px;
+            color: #28385c;
+          }
+        }
+        .stores {
+          display: flex;
+          flex-direction: row;
+          gap: 15px;
+        }
+        .avatar {
+          display: flex;
+          flex-direction: row;
+          gap: 5px;
+          align-items: center;
+          .downloads {
+            font-size: 13px;
+            font-weight: 500;
+            line-height: 19.36px;
+            color: #28385c;
+          }
+        }
+        .book {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          width: 143px;
+          height: 43px;
+          padding: 13px 12px 13px 12px;
+          gap: 8px;
+          border: 1px solid #27375b;
+          border-radius: 25px;
+          opacity: 0px;
+          background: #27375b;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 20px;
+          letter-spacing: 0.02em;
+          color: #ffffff;
+          outline: none;
+          cursor: pointer;
+          box-shadow: 0px 0px 0px 1px #27375b;
+          /* box-shadow: 0px 1px 1px 0px #0000001a; */
+        }
+      }
+      .side {
+        .pictures {
+          width: ${(props) => (props?.reduce ? "620px" : "590px")};
+          height: ${(props) => (props?.reduce ? "470px" : "auto")};
+        }
+      }
+    }
+    .bottom {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+      .trusted {
+        font-size: 17px;
+        font-weight: 400;
+        line-height: 24.2px;
+        text-align: left;
+        color: #5a6376cc;
+      }
+      .sliding {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        width: 60%;
+      }
+    }
+  }
 `;
 
 export default FirstIndustryComponent;
